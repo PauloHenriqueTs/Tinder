1cfe2ff90ba9dc223d4a7a4348325208ab39d4bb
Paulo <henriquepaulo991@gmail.com>
2019-03-24 20:03:33 -0300

    add controller and web packages

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Steps to run this project:
 1. Run `npm i` command
 2. Setup database settings inside `ormconfig.json` file
 3. Run `npm start` command
+   yarn upgrade-interactive --latest
+   git push origin -f
+   git reset --hard
---
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { RegisterController } from "@tinder/controller";
+
+import { RegisterView } from "./ui/RegisterView";
+
+// container -> view
+// container -> connector -> view
+// controller -> connector -> view
+
+export class RegisterConnector extends React.PureComponent {
+  render() {
+    return (
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
+    );
+  }
+}
